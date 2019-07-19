e9381b49204613495a7e3f3e8f049affe5fa2327
Adam Bolding-Jones <anb1@sfu.ca>
2019-07-19 09:21:08 -0700

    Improved the GetShortestWait... function. Still broken, it thinks 11:20PM has the shortest wait and the DateTime ToString is broken.

--- FerryBusConnector.cs
+++ FerryBusConnector.cs
@@ -19,29 +19,31 @@ namespace FerryBusConnector
             int choice = 0;
             do 
             {
-                Console.WriteLine("Choose mode: 1) Get shortest overall wait time 2) Choose a specific sailing.");
+                Console.WriteLine("Choose mode: 1) Get shortest overall wait time 2) Get wait info for a specific sailing.");
                 choice = Convert.ToInt32(Console.ReadLine());
             }
             while (choice != 1 && choice != 2);
 
             if (choice == 1)
             {
-                Console.WriteLine(GetShortestWaitMessage());
+                string sailing = GetShortestWaitFerry();
+                Console.WriteLine(
+                    @"The shortest wait will be on the {0} sailing from Horseshoe Bay. You'll arrive in Langdale at around {1} and 
+    can take an express bus at {2} or a normal bus at {3}.",
+                    sailing,
+                    DateTimeToString(GetFerryArrival(sailing, FerryBusConnector.ferryDurationInMin)),
+                    DateTimeToString(DetermineBus(sailing, isExpressBus: true)),
+                    DateTimeToString(DetermineBus(sailing, isExpressBus: false)));
             }
             else if (choice == 2)
             {
                 Console.Write("Choose which sailing you'll be on: ");
-                for (int i = 1; i < ferryDepartures.Length; i++)
+                for (int i = 0; i < ferryDepartures.Length; i++)
                 {
-                    if (i < ferryDepartures.Length)
-                    {
-                        Console.Write("{0}. {1} ", i, ferryDepartures[i-1]);
-                    }
-                    else
-                    {
-                        Console.WriteLine("{0}. {1}", i, ferryDepartures[i-1]);
-                    }
+                    Console.Write("{0}. {1} ", i+1, ferryDepartures[i]);
                 }
+                Console.WriteLine();
+                
                 int sailingNo = Convert.ToInt32(Console.ReadLine());
                 string sailing = ferryDepartures[sailingNo - 1];
                 Console.WriteLine(
@@ -54,52 +56,32 @@ namespace FerryBusConnector
             }
         }
 
-        static string GetShortestWaitMessage()
+        static string GetShortestWaitFerry()
         {           
-            DateTime? busDepWithShortestWait = null;
-            string shortestBusWaitFerry = null;
-            int? shorestWaitTimeInMinutes = null;
-            bool? shortestWaitIsExpress = null;
-            foreach(string ferryDeparture in FerryBusConnector.ferryDepartures)
+            string shortestWaitFerry = null;
+            TimeSpan shortestWait = new TimeSpan(24,0,0);
+            foreach(string sailing in FerryBusConnector.ferryDepartures)
             {
-                DateTime expressBusDep = DetermineBus(ferryDeparture, isExpressBus: true);
-                DateTime normalBusDep = DetermineBus(ferryDeparture, isExpressBus: false);
-                DateTime ferryArrival = GetFerryArrival(ferryDeparture, FerryBusConnector.ferryDurationInMin);
+                DateTime ferryArrival = GetFerryArrival(sailing, FerryBusConnector.ferryDurationInMin);
 
-                TimeSpan expressBusWait = expressBusDep.Subtract(ferryArrival);
+                DateTime expBusDep = DetermineBus(sailing, isExpressBus: true);
+                TimeSpan expBusWait = expBusDep.Subtract(ferryArrival);
+                if (expBusWait < shortestWait)
+                {
+                    shortestWait = expBusWait;
+                    shortestWaitFerry = sailing;
+                }
+
+                DateTime normalBusDep = DetermineBus(sailing, isExpressBus: false);
                 TimeSpan normalBusWait = normalBusDep.Subtract(ferryArrival);
-                DateTime earliestDep;
-                if (expressBusWait < normalBusWait)
+                if (normalBusWait < shortestWait)
                 {
-                    earliestDep = expressBusDep;
-                    shorestWaitTimeInMinutes = expressBusWait.Minutes;
-                    shortestWaitIsExpress = true;
-                }
-                else 
-                {
-                    earliestDep = normalBusDep;
-                    shorestWaitTimeInMinutes = normalBusWait.Minutes;
-                    shortestWaitIsExpress = false;
-                }
-                
-
-                if (busDepWithShortestWait == null || earliestDep < busDepWithShortestWait)
-                {
-                    busDepWithShortestWait = earliestDep;
-                    shortestBusWaitFerry = ferryDeparture;
+                    shortestWait = normalBusWait;
+                    shortestWaitFerry = sailing;
                 }
             }
-            if (busDepWithShortestWait == null || shortestBusWaitFerry == null || shorestWaitTimeInMinutes == null || shortestWaitIsExpress == null)
-            {
-                throw new Exception("Unable to determine shortest wait");
-            }
 
-            return String.Format(
-                "Take the {0} ferry, then take the {1} {2} bus. You will wait for {3} minutes.",
-                 shortestBusWaitFerry,
-                 busDepWithShortestWait,
-                 (bool)shortestWaitIsExpress ? "express" : "non",
-                 shorestWaitTimeInMinutes);
+            return shortestWaitFerry;
         }
 
         static DateTime GetFerryArrival(string departure, int voyageDuration)
